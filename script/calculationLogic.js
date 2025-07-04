d67c7e5c650abe65ed1cdd103b06282153d003e4
Abduselam Awel <abdselam676@gmail.com>
2025-07-04 11:42:47 +0300

    Add postfixer
    Basic infix to postfix changer has been added

--- script/calculationLogic.js
+++ script/calculationLogic.js
@@ -25,3 +25,60 @@ const separator = (text)=> {
 	return result;
 }
 
+const postfixer = (infix, output = [])=> {
+	const presedence = new Map([['+', 1], ['-', 1], ['×', 2], ['÷', 2]]);
+	const [stack,
+		bracketStack] = [[],
+		[]];
+	let i = 0;
+	let isBracket = 0;
+	for (const elem of infix) {
+		i++;
+		if (elem === '(') isBracket++;
+		if (isBracket !== 0) {
+			if (elem === ')') {
+				bracketStack.reverse().forEach((each)=> {
+					output.push(each);
+				})
+				bracketStack.splice(0, bracketStack.length);
+				isBracket = false;
+			} else {
+				if (typeof(elem) === 'number')
+					output.push(elem);
+				else {
+					let j = bracketStack.length - 1;
+					if (bracketStack.length === 0 || presedence.get(bracketStack[j]) <
+						presedence.get(elem)) {
+						if (elem !== '(')
+							bracketStack.push(elem);
+					} else {
+						const oprtr = bracketStack.pop();
+						output.push(oprtr);
+						bracketStack.push(elem);
+					}
+				}
+			}
+		} else {
+
+			if (typeof(elem) === 'number')
+				output.push(elem);
+			else {
+				let j = stack.length - 1;
+				if (stack.length === 0 || presedence.get(stack[j]) < presedence.get(elem))
+					stack.push(elem);
+				else {
+					const oprtr = stack.pop();
+					output.push(oprtr);
+					stack.push(elem);
+				}
+			}
+		}
+		if (i === infix.length) {
+			for (let k = stack.length-1; k >= 0; k--) {
+				output.push(stack[k]);
+			}
+			delete stack;
+		}
+	}
+	return output;
+}
